0933114190af141056ef09bcaeea45145775d1cf
Rosana Rezende <rezende_rosana@hotmail.com>
2022-12-02 21:47:01 -0300

    adiciona testes para validações e método customizado

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is invalid if the level is not between 1 and 99" do
+    user = User.create(nickname: "John", kind: :wizard, level: 100)
+    expect(user).to be_invalid
+  end
+
+  it "returns the correct hero title" do
+    user = User.new(kind: "knight", nickname: "Sir Lancelot", level: 42)
+    expect(user.title).to eq("knight Sir Lancelot #42")
+  end
 end
