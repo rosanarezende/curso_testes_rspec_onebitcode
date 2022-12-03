ae057bbefe2894305a513a0a7735f38de912df14
Rosana Rezende <rezende_rosana@hotmail.com>
2022-12-03 14:54:12 -0300

    criando um controller básico ==> rails g controller users index create --no-helper --no-assets --no-controller-specs --no-view-specs --skip-routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+# Criando um controller básico
+# rails g controller users index create --no-helper --no-assets --no-controller-specs --no-view-specs --skip-routes
+
+class UsersController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
