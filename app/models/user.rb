7fd5b989c424a5aed1ff98c87a1127630298c748
Rosana Rezende <rezende_rosana@hotmail.com>
2022-12-02 21:23:57 -0300

    adiciona validações e método customizado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   enum kind: [ :knight, :wizard ]
+  # validações
+  validates :level, numericality: { greater_than: 0, less_than_or_equal_to: 99 }
+
+  # método customizado
+  def title
+    "#{self.kind} #{self.nickname} ##{self.level}"
+  end
 end
