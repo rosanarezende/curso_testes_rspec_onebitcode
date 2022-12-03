42433d16588ea42ee099b80ef8c83554f6d5b7ab
Rosana Rezende <rezende_rosana@hotmail.com>
2022-12-03 13:52:47 -0300

    adiciona ffaker - gem que permite gerar valores aleatórios ==>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,27 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is invalid if the level is not between 1 and 99" do
-    user = User.create(nickname: "John", kind: :wizard, level: 100)
-    expect(user).to be_invalid
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(100..9999)
+
+    # não usar create pois ele já salva no banco de dados
+    # new só cria uma instância do objeto
+    user = User.new(nickname: nickname, kind: kind, level: level)
+
+    expect(user).to_not be_valid
   end
 
   it "returns the correct hero title" do
-    user = User.new(kind: "knight", nickname: "Sir Lancelot", level: 42)
-    expect(user.title).to eq("knight Sir Lancelot #42")
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+
+    # agora colocamos um level válido
+    level = FFaker::Random.rand(1..99)
+
+    # aqui usamos o create pois queremos salvar no banco de dados
+    user = User.create(nickname: nickname, kind: kind, level: level)
+
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
   end
 end
