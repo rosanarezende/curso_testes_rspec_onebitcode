3c8c12afba74a02761729ab9223d6b95a4046570
Rosana Rezende <rezende_rosana@hotmail.com>
2022-12-03 15:05:26 -0300

    adicionar métodos de listar e criar usuários no controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,21 @@
 # rails g controller users index create --no-helper --no-assets --no-controller-specs --no-view-specs --skip-routes
 
 class UsersController < ApplicationController
+  # lista todos os usuários
   def index
+    @users = User.all
   end
 
+  # permite a criação de um novo usuário
   def create
+    @user = User.new(user_params)
+    # redireciona para a página de usuários (index)
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :level, :kind)
   end
 end
