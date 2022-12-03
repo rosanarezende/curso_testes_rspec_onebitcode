4701dd1920c675e96f1f97fe9826ade840e86500
Rosana Rezende <rezende_rosana@hotmail.com>
2022-12-03 15:06:23 -0300

    alterar a factory para evitar incompatibilidade nos testes

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,10 @@ FactoryBot.define do
   factory :user do
     # poderia usar FFaker::Name.first_name, mudamos apenas para conhecer outros métodos do FFaker
     nickname { FFaker::Lorem.word } 
-    kind { %i[knight wizard].sample }
+
+    # com o %i ele devolve um simbolo, mas para não termos uma inconsistência nos testes precisamos devolver uma string usando %w
+    kind { %w[knight wizard].sample }
+
     level { FFaker::Random.rand(1..99) }
   end
 end
